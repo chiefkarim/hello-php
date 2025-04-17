e71434e8ff6f4d6eec51855340889b5707e1753e
chiefkarim <krimotop2016@gmail.com>
2025-04-17 10:46:26 +0100

    refactor: extract logout logic into it's own function

--- Core/functions.php
+++ Core/functions.php
@@ -27,3 +27,16 @@ function view($path, $arguments = [])
 
     require base_path('/views/' . $path);
 }
+
+function logout()
+{
+    // get session params
+    $params = session_get_cookie_params();
+    // deelete session data and destroy it
+    session_unset();
+    session_destroy();
+    //remove user cookie
+    setcookie(session_name(), '', time() - 4200, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+
+
+}
--- controllers/logout/destroy.php
+++ controllers/logout/destroy.php
@@ -1,12 +1,5 @@
 <?php
 
-// get session params
-$params = session_get_cookie_params();
-// deelete session data and destroy it
-session_unset();
-session_destroy();
-//remove user cookie
-setcookie(session_name(), '', time() - 4200, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-
+logout();
 // redirect to home page
 header("Location: /");
